ddd53a2bf6e2e8486d2a2692200bc71e78094419
AlexEFpro <alejandro.salinas@udgvirtual.udg.mx>
2025-01-25 13:01:55 -0600

    Initial Commit

---
+++ SwiftDataCustomContainer/SwiftDataCustomContainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataCustomContainerApp.swift
+//  SwiftDataCustomContainer
+//
+//  Created by Alejandro  Salinas Ramirez on 25/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataCustomContainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
